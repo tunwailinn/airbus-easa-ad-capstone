e0c233c1426f877261204d414185cff4434d060e
tunwailinn <95237518+tunwailinn@users.noreply.github.com>
2026-08-12 15:06:39 +0530

    Add Layer C provider package

---
+++ full_corpus_pipeline/layer_c/providers/__init__.py
@@ -0,0 +1,5 @@
+"""Direct hosted-model providers for Layer C development and evaluation."""
+
+from .deepseek import DeepSeekProvider, DEEPSEEK_BASE_URL, DEEPSEEK_MODEL
+
+__all__ = ["DeepSeekProvider", "DEEPSEEK_BASE_URL", "DEEPSEEK_MODEL"]
